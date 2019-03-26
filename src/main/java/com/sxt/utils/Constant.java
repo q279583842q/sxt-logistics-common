d40868b7d88af4850d1e18ef8ab34698378624c2
dpb <dengpbs@163.com>
2019-03-26 11:38:47 +0800

    添加了常量

---
+++ src/main/java/com/sxt/utils/Constant.java
@@ -0,0 +1,16 @@
+package com.sxt.utils;
+
+public class Constant {
+
+	public static final String PAGEHELP_MODEL = "pageModel";
+
+	public static final String ROLE_SALESMAN = "业务员";
+	public static final String ROLE_OPERATOR = "操作员";
+	public static final String ROLE_ADMIN = "管理员";
+
+	public static final String BASIC_COMMON_INTERVAL = "常用区间";
+	public static final String BASIC_FETCH_TYPE = "取件方式";
+	public static final String BASIC_FREIGHT_TYPE = "货运方式";
+	public static final String BASIC_PAYMENT_TYPE = "付款方式";
+	public static final String BASIC_UNIT = "单位";
+}
